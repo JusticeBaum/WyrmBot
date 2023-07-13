ae578587eedeaeb80a3bb1a183a096687e662694
Justice Baum <justicebaum12@gmail.com>
2023-07-12 18:36:57 -0700

    Added ability to join voice channel

--- src/music.py
+++ src/music.py
@@ -1,16 +1,61 @@
+import asyncio
 import discord
 from discord.ext import commands
-from youtube_dl import YoutubeDL
+import youtube_dl
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0', 
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class Source(discord.PCMVolumeTransformer):
+    def __init__(self, source, data, volume = 0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    # Get a streamable object from a url
+    async def from_url(cls, url, loop = None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+        # if playlist
+        if 'entries' in data:
+            # take first item
+            data = data['entries'][0]
+        
+        filename = data['url']
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        
 # Class that provides music player functionality
 class Player(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.is_playing = False
-        self.is_paused = False
-        self.queue = []
-        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
-        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-        self.vc = None
-    
+    @commands.command(name = "join", aliases = ["j"], help = "Joins caller's current voice channel")
+    async def join(self, ctx):
+
+        channel = ctx.message.author.voice.channel
+        try:
+            await channel.connect()
+        except Exception:
+            await ctx.send("Caller must be in a voice channel")
+            return
